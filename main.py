9316c1c86eb7cc0dd05cce0f45512b4640d98fcd
kevinnguyen2000 <kevinnguyen3047@gmail.com>
2021-06-06 15:41:59 +1000

    done sign up and log in pages

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+# route for home page
 @app.route("/")
 def home():
     return render_template("home.html")
+
+# route for sign up page
+@app.route("/signup/", methods=["POST", "GET"])
+def signup():
+
+    if request.method == 'POST':
+        return render_template("signup.html")
+    else:
+        return render_template("signup.html")
+
+# route for log in page
+@app.route("/login/", methods=["POST", "GET"])
+def login():
+    
+    if request.method == 'POST':
+        return render_template("login.html")
+    else:
+        return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
