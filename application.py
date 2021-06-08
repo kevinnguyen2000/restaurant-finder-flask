702efa6f404a9a50e5b09204247d08b8e5a7b164
Christian Adriano <s3783461@student.rmit.edu.au>
2021-06-08 22:43:22 +1000

    google login functional, formatting required

--- application.py
+++ application.py
@@ -1,12 +1,37 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from dotenv import load_dotenv
+from flask_dance.contrib.google import make_google_blueprint, google
+import logging
 
 application = Flask(__name__)
 application.secret_key = "top_secret_key"
+load_dotenv()
+login_client_id = os.getenv('GOOGLE_CLIENT_ID')
+login_secret = os.getenv('GOOGLE_CLIENT_SECRET')
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
+
+blueprint = make_google_blueprint(
+    client_id=login_client_id,
+    client_secret=login_secret,
+    reprompt_consent=True,
+    scope=["profile","email"]
+)
+
+application.register_blueprint(blueprint, url_prefix="/login/")
 
 # route for home page
-@application.route("/")
+@application.route("/") 
 def home():
-    return render_template("home.html")
+    google_data = None
+    user_info_endpoint = '/oauth2/v2/userinfo'
+    if google.authorized:
+        google_data = google.get(user_info_endpoint).json()
+
+    return render_template("home.html", google_data=google_data,fetch_url=google.base_url + user_info_endpoint)
+
+
 
 # route for sign up page
 @application.route("/signup/", methods=["POST", "GET"])
@@ -22,9 +47,9 @@ def signup():
 def login():
     
     if request.method == 'POST':
-        return render_template("login.html")
+        return redirect(url_for('google.login'))
     else:
-        return render_template("login.html")
+        return redirect(url_for('google.login'))
 
 # route for account page
 @application.route("/account/", methods=["POST", "GET"])
